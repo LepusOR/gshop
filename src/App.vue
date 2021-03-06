5f51e568f8462ac6e8a5be836adc04c35af47f77
LepusOR <magic9319@163.com>
2021-03-06 15:28:21 +0800

    init-app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="appContainer">
+    <router-view></router-view>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default{
+    name:'app',
+    components:{FooterGuide}
+  }
+</script>
+
+<style lang="stylus">
+  @import "./common/stylus/mixins.styl"
+  
+</style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,51 @@
+<template>
+   <div id="footerContainer">
+       <div :class="{active:$route.path==='/msite'}" @click="goPath('./msite')" class="guideItem">
+           <i class="iconfont iconshouye"></i><span>首页</span>
+       </div>
+       <div :class="{active:$route.path==='/search'}" @click="goPath('./search')" class="guideItem">
+           <i class="iconfont iconsousuo"></i><span>搜索</span>
+       </div>
+       <div :class="{active:$route.path==='/order'}" @click="goPath('./order')" class="guideItem">
+           <i class="iconfont icondingdan1"></i><span>订单</span>
+       </div>
+       <div :class="{active:$route.path==='/profile'}" @click="goPath('./profile')" class="guideItem">
+           <i class="iconfont iconwode"></i><span>我的</span>
+       </div>
+   </div>
+</template>
+
+<script>
+   export default {
+       methods:{
+           goPath(path){
+               this.$router.push(path)
+           }
+       }
+   }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/mixins.styl"
+    #footerContainer
+        display flex
+        top-border-1px(#eee)
+        position fixed
+        left 0
+        bottom 0
+        width 100%
+        height 50px
+        box-sizing border-box
+        background #ffffff
+        .guideItem
+            width 25%
+            height 50px
+            text-align center
+            display flex 
+            flex-direction column
+            &.active
+                color $green
+            .iconfont 
+                font-size 22px
+                line-height 30px
+</style>
